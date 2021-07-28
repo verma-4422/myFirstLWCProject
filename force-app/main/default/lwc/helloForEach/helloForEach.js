e081beb0fd5d1f5650b186ec657f541ddb7fcdcf
GAGAN <verma4422@gmail.com>
2021-07-28 00:22:59 -0400

    commit

---
+++ force-app/main/default/lwc/helloForEach/helloForEach.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloForEach extends LightningElement {
+
+    contacts=[
+        {
+            id:1,
+            name:'GAGAN',
+            title:'VP of Engineering'
+        },
+        {
+            id:1,
+            name:'Kunal',
+            title:'VIP of Engineering'
+        },
+        {
+            id:1,
+            name:'TATTA',
+            title:'VP of Engineering'
+        }
+    ]
+}
---
+++ force-app/main/default/lwc/iteratorList/iteratorList.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+
+export default class IteratorList extends LightningElement {
+    contacts=[
+        {
+            id:1,
+            name:'GAGAN',
+            title:'VP of Engineering'
+        },
+        {
+            id:1,
+            name:'Kunal',
+            title:'VIP of Engineering'
+        },
+        {
+            id:1,
+            name:'TATTA',
+            title:'VP of Engineering'
+        }
+    ]
+
+
+
+
+}
